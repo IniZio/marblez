6cd5d29efc684d30a8c1fa5f95a502f47ed63b56
inizio <newman.kcchow@gmail.com>
2020-06-04 00:02:47 +0800

    Editable datepicker

--- client/src/components/DatePicker.tsx
+++ client/src/components/DatePicker.tsx
@@ -1,6 +1,7 @@
-import React from 'react'
-import {format, addDays} from 'date-fns'
-import { IconButton, Flex } from '@chakra-ui/core';
+import React, { useState, useCallback, useEffect } from 'react'
+import {format, addDays, isValid, parse} from 'date-fns'
+import { IconButton, Flex, Editable, EditablePreview, EditableInput, Input } from '@chakra-ui/core';
+import InputMask from 'react-input-mask';
 
 export interface DatePickerProps {
   value?: Date;
@@ -8,10 +9,24 @@ export interface DatePickerProps {
 }
 
 function DatePicker({value = new Date(), onChange, ...props}: DatePickerProps) {
+  const [date, setDate] = useState(format(value, 'MM/dd/yyyy'));
+  useEffect(() => { setDate(format(value, 'MM/dd/yyyy')); }, [value])
+  const _onChange = useCallback((e) => {
+    setDate(e.target.value);
+    const parsedDate = parse(e.target.value, 'MM/dd/yyyy', new Date());
+    if (isValid(parsedDate)) {
+      onChange(parsedDate);
+    }
+  }, [date, setDate])
+  
   return (
     <Flex p={3} alignItems="center" justifyContent="center" {...props}>
       <IconButton aria-label="yesterday" icon="arrow-back" onClick={() => onChange(addDays(value, -1))} />
-      <Flex flex={1} alignItems="center" justifyContent="center">{format(value, 'MM/dd/yyyy')}</Flex>
+      <Flex flex={1} alignItems="center" justifyContent="center">
+        <InputMask mask="99/99/9999" value={date} onChange={_onChange}>
+          {() => <Input textAlign="center" mx={3} />}
+        </InputMask>
+      </Flex>
       <IconButton aria-label="tomorrow" icon="arrow-forward" onClick={() => onChange(addDays(value, 1))} />
     </Flex>
   )
