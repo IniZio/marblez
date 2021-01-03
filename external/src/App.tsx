ded27a1813c812e25a1217a247afd91ee27fc253
inizio <newman.kcchow@gmail.com>
2021-01-03 10:48:12 +0800

    Adds twin for tailwind

--- external/src/App.tsx
+++ external/src/App.tsx
@@ -1,14 +1,17 @@
 import React from 'react';
+import { GlobalStyles } from 'twin.macro'
+
 import './App.css';
+import ExampleButton from './components/ExampleButton';
 import Form from './Form';
 
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <Form />
-      </header>
+      <GlobalStyles />
+      <ExampleButton isPrimary>Hello</ExampleButton>
+      <Form />
     </div>
   );
 }
