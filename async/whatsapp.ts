3af0d05c52f7764a9ce24fc46048158a4b5347b9
IniZio <newman.kcchow@gmail.com>
2021-09-03 19:59:41 +0800

    fix(whatsapp): Try to reconnect on takeover, also bucket typo

--- async/whatsapp.ts
+++ async/whatsapp.ts
@@ -1,5 +1,5 @@
 import * as fs from 'fs'
-import { MessageType, WAChat, WAChatUpdate, WAConnection } from '@adiwajshing/baileys'
+import { MessageType, ReconnectMode, WAChat, WAChatUpdate, WAConnection } from '@adiwajshing/baileys'
 import mime from "mime-types"
 import supabase from "./services/supabase"
 import googleDriveRepository from "./respository/google-drive"
@@ -12,6 +12,9 @@ function chatIsMissMarble(chat: WAChat | WAChatUpdate) {
 export async function connectToWhatsApp () {
   await googleDriveRepository.init()
   const conn = new WAConnection()
+
+  conn.autoReconnect = ReconnectMode.onAllErrors
+
   // called when WA sends chats
   // this can take up to a few minutes if you have thousands of chats!
   // conn.on('chats-received', async ({ hasNewChats }) => {
@@ -68,45 +71,29 @@ export async function connectToWhatsApp () {
     }
 })
 
-  conn.on("message-new", async (m) => {
-    if (!m.message) {
-      return;
-    }
-
-    const [messageType] = Object.keys(m.message)
-
-    if (messageType === MessageType.image) {
-      // const buffer = await conn.downloadMediaMessage(m)
-      // @ts-expect-error
-      const savedFilename = await conn.downloadAndSaveMediaMessage(m)
-      console.log(m.key.remoteJid + " sent media, saved at: " + savedFilename)
-    }
-  })
-
   conn.on ('open', async () => {
     // save credentials whenever updated
     console.log (`credentials updated!`)
     const authInfo = conn.base64EncodedAuthInfo() // get all the auth info we need to restore this session
     const stringifiedAuthInfo = JSON.stringify(authInfo, null, '\t');
 
-    await supabase.storage.createBucket("whatsapp-auth-nfo");
+    await supabase.storage.createBucket("whatsapp-auth-info");
 
     // TODO: encrypt auth_info before upload
-    await supabase.storage.from("whatsapp-auth-nfo").upload("whatsapp_auth_info.json", stringifiedAuthInfo)
+    await supabase.storage.from("whatsapp-auth-info").upload("whatsapp_auth_info.json", stringifiedAuthInfo)
 
     fs.writeFileSync('./whatsapp_auth_info.json', stringifiedAuthInfo) // save this info to a file
   })
 
   try {
-    const { data } = await supabase.storage.from("whatsapp-auth-nfo").download("whatsapp_auth_info.json")
-    console.log("=== whatsapp", await data.text())
+    const { data } = await supabase.storage.from("whatsapp-auth-info").download("whatsapp_auth_info.json")
     fs.writeFileSync('./whatsapp_auth_info.json', await data.text())
   } catch {}
 
   try {
     conn.loadAuthInfo ('./whatsapp_auth_info.json')
   } catch (error) {
-    console.log("=== error", error)
+    console.log("Failed to load whatsapp authinfo", error)
   }
   await conn.connect ()
 }
