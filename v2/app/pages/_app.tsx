037f419047d6c2ef32bfaef5479323eaf3b8c434
IniZio <newman.kcchow@gmail.com>
2023-01-30 22:31:08 +0800

    Remove some unused deps and code

--- v2/app/pages/_app.tsx
+++ v2/app/pages/_app.tsx
@@ -1,9 +1,6 @@
-import LoginForm from "app/auth/components/LoginForm"
 import "app/core/styles/index.css"
 import {
   AppProps,
-  AuthenticationError,
-  AuthorizationError,
   ErrorBoundary,
   ErrorComponent,
   ErrorFallbackProps,
@@ -23,16 +20,6 @@ export default function App({ Component, pageProps }: AppProps) {
   )
 }
 
-function RootErrorFallback({ error, resetErrorBoundary }: ErrorFallbackProps) {
-  if (error instanceof AuthenticationError) {
-    return <LoginForm onSuccess={resetErrorBoundary} />
-  } else if (error instanceof AuthorizationError) {
-    return (
-      <ErrorComponent
-        statusCode={error.statusCode}
-        title="Sorry, you are not authorized to access this"
-      />
-    )
-  }
+function RootErrorFallback({ error }: ErrorFallbackProps) {
   return <ErrorComponent statusCode={error.statusCode || 400} title={error.message || error.name} />
 }
