5ac4eab7ebef58f253da104da4dca4045d36b5b3
Newman Chow <newman@oursky.com>
2023-06-22 12:04:58 +0800

    Add onesignal

--- server/app/pages/_app.tsx
+++ server/app/pages/_app.tsx
@@ -6,9 +6,21 @@ import {
   ErrorFallbackProps,
   useQueryErrorResetBoundary,
 } from "blitz"
+import { useEffect } from "react"
+import OneSignal from "react-onesignal"
 
 export default function App({ Component, pageProps }: AppProps) {
   const getLayout = Component.getLayout || ((page) => page)
+
+  useEffect(() => {
+    ;(async () => {
+      await OneSignal.init({
+        appId: "8972ff5a-8050-4c42-b0db-3940884fafdf",
+        allowLocalhostAsSecureOrigin: true,
+      })
+      OneSignal.showSlidedownPrompt()
+    })()
+  }, [])
 
   return (
     <ErrorBoundary
